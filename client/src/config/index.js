c6c4b52e233ca2162ebaa9f8b219ffb5bee8ad5a
zhengjinshou <zhengjinshou@tenwit.com>
2021-06-10 20:18:25 +0800

    修改版本号

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,5 @@
-
 // export const likeImgInCloud = "cloud://develop-longzi.6465-develop-longzi-1302443537/like.jpg"
-export const likeImgInCloud = "cloud://develop-longzi.6465-develop-longzi-1302443537/1/qrcode.jpg"
+export const likeImgInCloud =
+  "cloud://develop-longzi.6465-develop-longzi-1302443537/1/qrcode.jpg";
 
-export const version = '1.6.0'
+export const version = "1.6.3";
